280ce57f61202b6226f1c55d1387c2dfc06d6df7
hungthehunter <133243197+hungthehunter@users.noreply.github.com>
2025-01-08 19:30:20 +0700

    8/1/2025

--- backend/NVIDIA/NVIDIA/src/main/java/com/example/NVIDIA/model/PaymentMethod.java
+++
@@ -1,6 +0,0 @@
-package com.example.NVIDIA.model;
-
-public enum PaymentMethod {
-	 CREDIT_CARD,
-	 CASH_ON_DELIVERY
-}
--- backend/NVIDIA/NVIDIA/src/main/java/com/example/NVIDIA/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.NVIDIA.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-
-	
-	UserDetailsService userDetailsService();
-}
